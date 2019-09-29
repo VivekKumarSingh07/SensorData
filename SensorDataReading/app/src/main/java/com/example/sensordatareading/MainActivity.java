01ddf0d3eeec9cd263efe7abf22f6c4f3b94973d
VivekKumarSingh07 <singhvivekkumar203@gmail.com>
2019-09-29 11:08:12 +0530

    Magnetometer Accelerometer

--- SensorDataReading/app/src/main/java/com/example/sensordatareading/MainActivity.java
+++ SensorDataReading/app/src/main/java/com/example/sensordatareading/MainActivity.java
@@ -14,9 +14,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private Sensor mSensorProximity;
     private Sensor mSensorLight;
+    private Sensor mSensorAccelerometer;
+    private Sensor mSensorMagnetometer;
+
     private SensorManager mSensorManager;
+
     private TextView mTextSensorLight;
     private TextView mTextSensorProximity;
+    private TextView mtextSensorAccelerometer;
+    private TextView mTextSensorMagnetometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         mTextSensorLight = findViewById(R.id.label_light);
         mTextSensorProximity = findViewById(R.id.label_proximity);
+        mtextSensorAccelerometer= findViewById(R.id.label_accelerometer);
+        mTextSensorMagnetometer = findViewById(R.id.label_magnetometer);
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         String sensorError = getResources().getString(R.string.error_no_sensor);
 
@@ -39,6 +49,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         if (mSensorProximity == null)
             mTextSensorLight.setText(sensorError);
+
+        if(mSensorAccelerometer ==null)
+            mtextSensorAccelerometer.setText(sensorError);
+
+        if(mSensorMagnetometer ==null)
+            mTextSensorMagnetometer.setText(sensorError);
+
     }
 
     @Override
@@ -49,6 +66,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
         if (mSensorLight != null) {
             mSensorManager.registerListener(this, mSensorLight, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+        if(mSensorAccelerometer!=null){
+            mSensorManager.registerListener(this, mSensorAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(mSensorMagnetometer!=null){
+            mSensorManager.registerListener(this, mSensorMagnetometer, SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
 
@@ -61,15 +85,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     public void onSensorChanged(SensorEvent event) {
         int sensorType = event.sensor.getType();
-        float currentValue = event.values[0];
+        float []currentValue =  new float[3];
 
         switch (sensorType) {
             case Sensor.TYPE_LIGHT:
-                mTextSensorLight.setText(getResources().getString(R.string.label_light, currentValue));
+                currentValue[0] = event.values[0];
+                mTextSensorLight.setText(getResources().getString(R.string.label_light, currentValue[0]));
                 break;
             case Sensor.TYPE_PROXIMITY:
-                mTextSensorProximity.setText(getResources().getString(R.string.label_proximity, currentValue));
+                currentValue[0] = event.values[0];
+                mTextSensorProximity.setText(getResources().getString(R.string.label_proximity, currentValue[0]));
                 break;
+            case Sensor.TYPE_ACCELEROMETER:
+                for(int i=0; i<3; i++){
+                    currentValue[i] = event.values[i];
+                }
+                mtextSensorAccelerometer.setText("accelerometer Values" + currentValue[0] + " " + currentValue[1] + " " + currentValue[2]);
+                break;
+            case Sensor.TYPE_MAGNETIC_FIELD:
+                for(int i=0; i<3; i++){
+                    currentValue[i] = event.values[i];
+                }
+                mTextSensorMagnetometer.setText("Magnetometer value:" + currentValue[0] + " " + currentValue[1] + " " + currentValue[2]);
         }
     }
 
